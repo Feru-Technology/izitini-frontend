9a3b0fa3f1ea1158661cf1271fe762ef51e355f3
Rama <nseorama@gmail.com>
2020-07-29 10:49:09 +0200

    fix orders slice

--- src/redux/order/order.interface.ts
+++ src/redux/order/order.interface.ts
@@ -1,42 +1,53 @@
 import { IAddress } from '../adress/adress.interface'
 import { IProduct } from '../products/product.interface'
 
+export interface HTTPError {
+    status: number
+    message: string
+}
+
 export interface IOrder {
     id: string
-    user_id: string
     status: string
+    user_id: string
     is_sample: boolean
     shipping_address_id: string
 }
 
 export interface IOrderItem {
     id: string
-    order_id: string
-    product_id: string
     shop_id: string
-    color_id: string
     size_id: string
-    quantity: string
     details: string
+    color_id: string
+    quantity: string
+    order_id: string
     product: IProduct
+    product_id: string
 }
 
 export interface ICart {
     id: string
-    user_id: string
     status: string
+    user_id: string
     is_sample: boolean
     order_items: [IOrderItem]
 }
 
 export interface IOrders {
     id: string
-    user_id: string
     status: string
-    is_sample: boolean
-    shipping_address_id: string
+    shop_id: string
+    user_id: string
+    isPaid: boolean
+    order_no: number
     address: IAddress
-    order_items: [IOrderItem]
+    sub_total: string
+    is_sample: boolean
+    paid_amount: number
+    shipping_cost: number
+    payment_method: string
+    shipping_method: number
+    order_items: IOrderItem[]
+    shipping_address_id: string
 }
-
-export interface HTTPError { status: number; message: string }
