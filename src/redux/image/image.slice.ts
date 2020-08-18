d47e7993eb69aad592dffd97933b29a42cdc1ae4
Rama <nseorama@gmail.com>
2020-08-18 19:45:55 +0200

    add image slice

---
+++ src/redux/image/image.slice.ts
@@ -0,0 +1,42 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { IImage, HTTPError } from './image.interfaces'
+
+
+export interface imageState {
+    image: IImage | null
+    isUploading: boolean
+    uploadError: Error | HTTPError | null
+}
+
+const initialState: imageState = {
+    isUploading: false,
+    uploadError: null,
+    image: null,
+}
+
+export const imageSlice = createSlice({
+    name: 'image',
+    initialState,
+    reducers: {
+        uploadingImage: (state) => {
+            state.isUploading = true
+            state.image = null
+            state.uploadError = null
+        },
+        uploadedImage: (state, { payload }) => {
+            state.uploadError = null
+            state.image = payload
+            state.isUploading = false
+        },
+        uploadFailed: (state, { payload }) => {
+            state.image = null
+            state.uploadError = payload
+            state.isUploading = false
+        }
+    },
+})
+
+// Action creators are generated for each case reducer function
+export const { uploadingImage, uploadedImage, uploadFailed } = imageSlice.actions
+
+export default imageSlice.reducer
