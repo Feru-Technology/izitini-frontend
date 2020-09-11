3c3e566b951ecb64e33a0b2a0c2dccf37c6d8695
Rama <nseorama@gmail.com>
2020-09-11 14:55:55 +0200

    add product color model

--- src/components/admin/product.tsx
+++ src/components/admin/product.tsx
@@ -380,6 +380,7 @@ const AdminProduct = () => {
                                                     hover:border-dark-blue hover:text-dark-blue hover:bg-blue-50'
                                                         onPointerOver={() => setShowColorDesc(true)}
                                                         onPointerLeave={() => setShowColorDesc(false)}
+                                                        onClick={() => setAddColor(true)}
                                                     >
                                                         <PlusIcon className='h-6 mx-auto' />
                                                     </div>
@@ -441,7 +442,7 @@ const AdminProduct = () => {
                                 md:p-6 lg:p-8 relative'>
 
                                         <MdOutlineCancel className='h-6 w-auto absolute top-1 right-1
-                                    text-gray-600 hover:text-dark-blue hover:shadow-lg'
+                                    text-gray-600 hover:text-dark-blue hover:shadow-lg cursor-pointer'
                                             onClick={() => setAddSize(false)} />
 
                                         <div className='mb-3 font-semibold text-lg md:text-xl lg:text-2xl text-center text-gray-600'>New Product Size</div>
@@ -510,6 +511,91 @@ const AdminProduct = () => {
                                                     }}
                                                 >
                                                     {!!isCreatingSize ? 'Creating...' : 'Create'}
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </Transition>
+
+                            {/* add color to a product */}
+                            <Transition show={!!addColor} className='absolute'>
+                                <div className='top-0 z-10 text-gray-500 bg-gray-700 opacity-50 w-screen min-h-screen'>
+                                </div>
+                                <div className='absolute top-1/4 w-full z-30 text-xs md:text-base'>
+                                    <div className='p-3 bg-white w-ful mx-6 md:w-2/4 lg:w-1/4 md:mx-auto rounded-md shadow-md
+                                md:p-6 lg:p-8 relative'>
+
+                                        <MdOutlineCancel className='h-6 w-auto absolute top-1 right-1
+                                    text-gray-600 hover:text-dark-blue hover:shadow-lg cursor-pointer'
+                                            onClick={() => setAddColor(false)} />
+
+                                        <div className='mb-3 font-semibold text-lg md:text-xl lg:text-2xl text-center text-gray-600'>New Product Color</div>
+                                        <div className='container'>
+                                            <Transition
+                                                show={!!colorError}
+                                            >
+                                                <p className='p-4 mb-4 bg-red-100 border border-red-700 text-red-700 text-center '>{colorError?.message}</p>
+
+                                            </Transition>
+                                        </div>
+                                        <form>
+
+                                            <div className=' w-full mb-3'>
+                                                <label
+                                                    className='block uppercase text-gray-600 text-xs font-bold mb-2'
+                                                    htmlFor='grid-text'
+                                                >
+                                                    Color
+                                                </label>
+                                                <input
+                                                    type='text'
+                                                    className='border border-gray-700 px-3 py-3 placeholder-gray-500 text-gray-600 bg-white rounded text-sm  focus:outline-none  w-full ease-linear transition-all duration-150'
+                                                    placeholder='Color'
+                                                    onChange={e => setColorName(e.target.value)}
+                                                />
+                                            </div>
+
+                                            <div className=' w-full mb-3'>
+                                                <label
+                                                    className='block uppercase text-gray-600 text-xs font-bold mb-2'
+                                                    htmlFor='grid-text'
+                                                >
+                                                    Price
+                                                </label>
+                                                <input
+                                                    type='text'
+                                                    className='border border-gray-700 px-3 py-3 placeholder-gray-500 text-gray-600 bg-white rounded text-sm  focus:outline-none  w-full ease-linear transition-all duration-150'
+                                                    placeholder='Brand'
+                                                    onChange={e => setPricePerColor(e.target.value)}
+                                                />
+                                            </div>
+
+                                            <div className=' w-full mb-3'>
+                                                <label
+                                                    className='block uppercase text-gray-600 text-xs font-bold mb-2'
+                                                    htmlFor='grid-text'
+                                                >
+                                                    Quantity
+                                                </label>
+                                                <input
+                                                    type='number'
+                                                    className='border border-gray-700 px-3 py-3 placeholder-gray-500 text-gray-600 bg-white rounded text-sm  focus:outline-none  w-full ease-linear transition-all duration-150'
+                                                    placeholder='Quantity'
+                                                    onChange={e => setColorQuantity(e.target.value)}
+                                                />
+                                            </div>
+                                            <div className='text-center mt-6'>
+                                                <button
+                                                    className='bg-dark-blue hover:bg-middle-blue text-white  text-sm font-bold uppercase px-6 p-3
+                                            rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 w-full ease-linear transition-all duration-150'
+                                                    type='button'
+                                                    onClick={(e) => {
+                                                        e.preventDefault()
+                                                        return createColor()
+                                                    }}
+                                                >
+                                                    {!!isCreatingColor ? 'Creating...' : 'Create'}
                                                 </button>
                                             </div>
                                         </form>
