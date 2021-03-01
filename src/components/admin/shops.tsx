31dc9cfb0ba83d71b6c573cd6c6326fa460de3dc
Rama <nseorama@gmail.com>
2021-03-01 12:05:55 +0200

    feed shops data

--- src/components/admin/shops.tsx
+++ src/components/admin/shops.tsx
@@ -18,7 +18,7 @@ const Shops = () => {
     // redux
     const dispatch = useDispatch()
 
-    const { isLoading, profile } = useSelector((state: RootState) => state.profile)
+    const { profile } = useSelector((state: RootState) => state.profile)
 
     const isStatic = useMediaQuery({
         query: '(min-width: 640px)',
@@ -32,113 +32,117 @@ const Shops = () => {
         fetch(dispatch, retrievedStores, retrievedStoreFailed, '/shop')
     }, [dispatch])
 
-    const { stores, error } = useSelector((state: RootState) => state.stores)
+    const { isLoading, stores, error } = useSelector((state: RootState) => state.stores)
     console.log(stores);
 
     return (
         <>
             {isLoading ? (<h1>loading ...</h1>)
-                : profile ?
-                    (
-                        <div className='flex h-screen overflow-hidden'>
-                            <SiderBar
+                : stores ? (
+                    <div className='flex h-screen overflow-hidden'>
+                        <SiderBar
+                            isClosed={isClosed}
+                            setIsClosed={setIsClosed}
+                            isStatic={isStatic}
+                        />
+
+                        <div className='relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden'>
+                            <Header
                                 isClosed={isClosed}
                                 setIsClosed={setIsClosed}
                                 isStatic={isStatic}
+                                name={'Admin'}
                             />
+                            <Transition
+                                appear={true}
+                                show={!isStatic && !isClosed}
+                                enter='transition-opacity duration-200'
+                                enterFrom='opacity-0'
+                                enterTo='opacity-50'
+                                leave='transition-opacity duration-200'
+                                leaveFrom='opacity-50'
+                                leaveTo='opacity-0'
+                            >
+                                <div className='fixed inset-0 bg-black opacity-60 z-10' />
+                            </Transition>
 
-                            <div className='relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden'>
-                                <Header
-                                    isClosed={isClosed}
-                                    setIsClosed={setIsClosed}
-                                    isStatic={isStatic}
-                                    name={'Admin'}
-                                />
-                                <Transition
-                                    appear={true}
-                                    show={!isStatic && !isClosed}
-                                    enter='transition-opacity duration-200'
-                                    enterFrom='opacity-0'
-                                    enterTo='opacity-50'
-                                    leave='transition-opacity duration-200'
-                                    leaveFrom='opacity-50'
-                                    leaveTo='opacity-0'
-                                >
-                                    <div className='fixed inset-0 bg-black opacity-60 z-10' />
-                                </Transition>
+                            {/* customer dashboard */}
 
-                                {/* customer dashboard */}
+                            <div className='px-2 md:px-6 lg:px-14 w-full'>
+                                <p className='font-bold my-3 text-sm md:mt-6 md:text-xl text-center underline'>Shops</p>
 
-                                <div className='px-2 md:px-6 lg:px-14 w-full'>
-                                    <p className='font-bold my-3 text-sm md:mt-6 md:text-xl text-center underline'>Shops</p>
-
-                                    <div className='w-full my-4 md:my-5 lg:my-6 '>
-                                        <table className='w-full border-gray-200 text-gray-600 border'>
-                                            <thead className=''>
-                                                <tr className='font-bold text-xs md:text-sm text-center border-b'>
-                                                    <th
-                                                        scope='col'
-                                                        className='
+                                <div className='w-full my-4 md:my-5 lg:my-6 '>
+                                    <table className='w-full border-gray-200 text-gray-600 border'>
+                                        <thead className=''>
+                                            <tr className='font-bold text-xs md:text-sm text-center border-b'>
+                                                <th
+                                                    scope='col'
+                                                    className='
                                                 w-2/5 py-3 lg:text-base
                                     '
-                                                    >Name</th>
-                                                    <th
-                                                        scope='col'
-                                                        className='
+                                                >Name</th>
+                                                <th
+                                                    scope='col'
+                                                    className='
                                                 py-3 lg:text-base
                                     '
-                                                    >Email</th>
-                                                    <th
-                                                        scope='col'
-                                                        className='
+                                                >Email</th>
+                                                <th
+                                                    scope='col'
+                                                    className='
                                                 py-3 lg:text-base
                                     '
-                                                    >Contacts</th>
-                                                    <th
-                                                        scope='col'
-                                                        className='
+                                                >Contacts</th>
+                                                <th
+                                                    scope='col'
+                                                    className='
                                                 py-3 lg:text-base
                                     '
-                                                    >Specialty</th>
-                                                </tr>
-                                            </thead>
+                                                >Specialty</th>
+                                            </tr>
+                                        </thead>
 
-                                            <tbody>
-                                                <tr className='text-center text-xs md:text-sm lg:text-base border-b text-gray-800'>
-                                                    <td className='py-3 '>
-                                                        <div className='md:flex items-center'>
-                                                            <div className='md:w-1/4 mx-3'>
-                                                                <img src='https://images.pexels.com/photos/834892/pexels-photo-834892.jpeg' alt='product' className='w-full' />
+                                        {stores.map((store) => {
+                                            const storeImage = store.shop_image_url || 'https://izitini-spaces.fra1.digitaloceanspaces.com/profile-pics/profile.png'
+                                            return (
+                                                <tbody>
+                                                    <tr className='text-center text-xs md:text-sm lg:text-base border-b text-gray-800'>
+                                                        <td className='py-3 '>
+                                                            <div className='md:flex items-center'>
+                                                                <div className='md:w-1/4 mx-3'>
+                                                                    <img src='https://images.pexels.com/photos/834892/pexels-photo-834892.jpeg' alt='product' className='w-full' />
+                                                                </div>
+                                                                <div className='md:w-2/4'>
+
+                                                                    <p className='font-normal text-sm'>
+                                                                        <span className=''>Shop Name</span>
+                                                                    </p>
+                                                                </div>
                                                             </div>
-                                                            <div className='md:w-2/4'>
-
-                                                                <p className='font-normal text-sm'>
-                                                                    <span className=''>Shop Name</span>
-                                                                </p>
-                                                            </div>
-                                                        </div>
-                                                    </td>
-                                                    <td className='py-3 '>
-                                                        <p className='font-normal text-sm'>email@gmail.com</p>
-                                                    </td>
-                                                    <td className='py-3 '>
-                                                        <p className='font-normal text-sm'>0786493807</p>
-                                                    </td>
-                                                    <td className='py-3 '>
-                                                        <p className='font-normal text-sm'>Electricity && Water</p>
-                                                    </td>
-                                                </tr>
-                                            </tbody>
-
-                                        </table>
-
-                                    </div>
+                                                        </td>
+                                                        <td className='py-3 '>
+                                                            <p className='font-normal text-sm'>email@gmail.com</p>
+                                                        </td>
+                                                        <td className='py-3 '>
+                                                            <p className='font-normal text-sm'>0786493807</p>
+                                                        </td>
+                                                        <td className='py-3 '>
+                                                            <p className='font-normal text-sm'>Electricity && Water</p>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>)
+                                        })
+                                        }
+                                    </table>
 
                                 </div>
+
                             </div>
                         </div>
-                    )
-                    : navigate('/signin')
+                    </div>
+
+                )
+                    : <div></div>
 
             }
         </>
--- src/redux/stores/store.interfaces.ts
+++ src/redux/stores/store.interfaces.ts
@@ -1,10 +1,14 @@
 export interface IStore {
-    id: string;
-    name: string;
-    owner: string;
-    about_shop: string;
-    shop_email: string;
-    shop_contact_no: string;
-    is_approved: boolean;
-};
-export interface HTTPError { status: number; message: string }
+    id: string
+    name: string
+    owner: string
+    about_shop: string
+    shop_email: string
+    is_approved: boolean
+    shop_contact_no: string
+    shop_image_url: string
+}
+export interface HTTPError {
+    status: number
+    message: string
+}
