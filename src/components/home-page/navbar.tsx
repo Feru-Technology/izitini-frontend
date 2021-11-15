bb47ce700c77bb4c8e8cb877689aaafaf12bade5
Rama <nseorama@gmail.com>
2021-11-15 15:04:36 +0200

    re structure thenav bar

--- src/components/home-page/navbar.tsx
+++ src/components/home-page/navbar.tsx
@@ -6,27 +6,36 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function NavBar() {
   return (
     <div className="">
-      <nav className="navbar navbar-expand-lg navbar-expand-md navbar-light ">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/Logo1.png" alt="logo" className='logo' />
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <form className="d-flex mx-5">
-              <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
-              <button type="button" className="btn btn-primary">
-                <FontAwesomeIcon icon={["fal", "coffee"]} />
-              </button>
+            <form className="container-fluid w-25">
+              <div className="input-group m2">
+                <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+
+                <span className="input-group-text" id="basic-addon1">
+                  <FontAwesomeIcon icon={["fal", "coffee"]} /></span>
+              </div>
             </form>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item mx-lg-5">
-                Sign in as a Customer
+              <li className="nav-item">
+                <p className="nav-link active" aria-current="page">Sign in as a Customer</p>
               </li>
-              <li className="nav-item mx-lg-5">
-                Sign in as a Seller
+              <li className="nav-item">
+                <p className="nav-link disabled" aria-disabled="true">Sign in as a Seller</p>
               </li>
             </ul>
           </div>
         </div>
-      </nav >
+      </nav>
+
+      <div className="container">
+        <h1>the second container</h1>
+      </div>
     </div>
   );
 }
