14b528c035b3655ae2bf53a015cf5d4d191ac04d
Rama <nseorama@gmail.com>
2021-11-16 22:13:17 +0200

    add sliderr

--- src/components/home-page/getIdea.tsx
+++ src/components/home-page/getIdea.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Carousel from 'react-multi-carousel';
 import "react-multi-carousel/lib/styles.css";
 import './home.scss';
 
@@ -10,13 +11,13 @@ function GetIdeaSection() {
             <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
                 <div className="carousel-inner">
                     <div className="carousel-item active">
-                        <img src="..." className="d-block w-100" alt="..." />
+                        <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/design/pexels-houzlook-com-3356416.jpg" className="d-block w-100" alt="..." />
                     </div>
                     <div className="carousel-item">
-                        <img src="..." className="d-block w-100" alt="..." />
+                        <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/design/pexels-julie-aagaard-2207894.jpg" className="d-block w-100" alt="..." />
                     </div>
                     <div className="carousel-item">
-                        <img src="..." className="d-block w-100" alt="..." />
+                        <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/design/pexels-mark-mccammon-1080721.jpg" className="d-block w-100" alt="..." />
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
