1fe6ce3f7efce651a80e9ef2e791c354d95b8c57
Rama <nseorama@gmail.com>
2021-11-16 10:06:31 +0200

    add category section

---
+++ src/components/home-page/categorySection.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './home.scss'
+function CategorySection() {
+    return (
+        <div className='category-section'>
+            <div className="card">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CategorySection;
--- src/components/home-page/home.tsx
+++ src/components/home-page/home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './home.scss';
 import NavBar from './navbar';
 import LoginSection from './loginSection';
+import CategorySection from './categorySection';
 // import {Link} from 'react-router-dom';
 
 function HomePage() {
@@ -9,6 +10,7 @@ function HomePage() {
     <div className='w-100'>
       <NavBar />
       <LoginSection />
+      <CategorySection />
     </div>
   );
 }
