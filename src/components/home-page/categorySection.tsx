f124739efb80cfa7224a474b486eecfba2e7ae87
Rama <nseorama@gmail.com>
2021-11-16 15:35:09 +0200

    add styling

--- src/components/home-page/categorySection.tsx
+++ src/components/home-page/categorySection.tsx
@@ -1,17 +1,100 @@
 import React from 'react';
-import './home.scss'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import './home.scss';
+
+
 function CategorySection() {
+
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 6
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 6
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
+
     return (
         <div className='raw category-section mt-5'>
-            <p className=''> Buy by category</p>
-            <div className="card col">
-                <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/Artboard%20%E2%80%93%202.png" className="card-img-top" alt="..." />
-                <div className="card-body border-top">
-                    <p className="card-text">building materials</p>
-                </div>
+            <p className='text-center fs-4 fw-bolder'> Buy by category</p>
+            <div className='center'>
+                <Carousel responsive={responsive}>
+                    <div className="card col">
+                        <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/Artboard%20%E2%80%93%202.png" className="card-img-top" alt="..." />
+                        <div className="card-body border-top">
+                            <p className="card-text">building materials</p>
+                        </div>
+                    </div>
+                    <div className="card col">
+                        <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/Artboard%20%E2%80%93%202.png" className="card-img-top" alt="..." />
+                        <div className="card-body border-top">
+                            <p className="card-text">building materials</p>
+                        </div>
+                    </div>
+                    <div className="card col">
+                        <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/Artboard%20%E2%80%93%202.png" className="card-img-top" alt="..." />
+                        <div className="card-body border-top">
+                            <p className="card-text">building materials</p>
+                        </div>
+                    </div>
+                    <div className="card col">
+                        <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/Artboard%20%E2%80%93%202.png" className="card-img-top" alt="..." />
+                        <div className="card-body border-top">
+                            <p className="card-text">building materials</p>
+                        </div>
+                    </div>
+                    <div className="card col">
+                        <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/Artboard%20%E2%80%93%202.png" className="card-img-top" alt="..." />
+                        <div className="card-body border-top">
+                            <p className="card-text">building materials</p>
+                        </div>
+                    </div>
+                    <div className="card col">
+                        <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/Artboard%20%E2%80%93%202.png" className="card-img-top" alt="..." />
+                        <div className="card-body border-top">
+                            <p className="card-text">building materials</p>
+                        </div>
+                    </div>
+                    <div className="card col">
+                        <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/Artboard%20%E2%80%93%202.png" className="card-img-top" alt="..." />
+                        <div className="card-body border-top">
+                            <p className="card-text">building materials</p>
+                        </div>
+                    </div>
+                    <div className="card col">
+                        <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/Artboard%20%E2%80%93%202.png" className="card-img-top" alt="..." />
+                        <div className="card-body border-top">
+                            <p className="card-text">building materials</p>
+                        </div>
+                    </div>
+                    <div className="card col">
+                        <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/Artboard%20%E2%80%93%202.png" className="card-img-top" alt="..." />
+                        <div className="card-body border-top">
+                            <p className="card-text">building materials</p>
+                        </div>
+                    </div>
+                    <div className="card col">
+                        <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/Artboard%20%E2%80%93%202.png" className="card-img-top" alt="..." />
+                        <div className="card-body border-top">
+                            <p className="card-text">building materials</p>
+                        </div>
+                    </div>
+                </Carousel>
             </div>
+
         </div>
     );
 }
-
 export default CategorySection;
