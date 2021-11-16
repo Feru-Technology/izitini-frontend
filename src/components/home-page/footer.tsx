d6035fa1fea210491beb424ed34bdb9594211e54
Rama <nseorama@gmail.com>
2021-11-16 23:32:58 +0200

    restructure footer

--- src/components/home-page/footer.tsx
+++ src/components/home-page/footer.tsx
@@ -7,13 +7,17 @@ function Footer() {
         <div className="footer my-1">
             <div className="container p-4 pb-0">
 
-                <div className='row'>
-                    <div className='col-lg-4 col-md-4 col-sm-6'></div>
+                <div className='row border-bottom'>
+                    <div className='col-lg-4 col-md-4 col-sm-6'>
+                        <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/Logo.png" alt="" />
+                        <p className="text-white">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                            invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo</p>
+                    </div>
                     <div className='col-lg-4 col-md-4 col-sm-6'></div>
                     <div className='col-lg-4 col-md-4 col-sm-6'></div>
                 </div>
 
-                <div className="row">
+                <div className="row mt-3">
 
                     <div className="col-lg-2 col-md-6 mb-4 mb-md-0">
                         <h5 className="text-white">Links</h5>
