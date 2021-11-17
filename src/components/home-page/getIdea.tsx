016c2e138d58bc636c9b52e8cb3acdce7d8e6707
Rama <nseorama@gmail.com>
2021-11-17 16:50:15 +0200

    add footer items

--- src/components/home-page/getIdea.tsx
+++ src/components/home-page/getIdea.tsx
@@ -1,33 +1,31 @@
 import React from 'react';
 import "react-multi-carousel/lib/styles.css";
+import SimpleImageSlider from "react-simple-image-slider";
 import './home.scss';
+
+
+const images = [
+    { url: "https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/design/pexels-houzlook-com-3356416.jpg" },
+    { url: "https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/design/pexels-julie-aagaard-2207894.jpg" },
+    { url: "https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/design/pexels-mark-mccammon-1080721.jpg" },
+];
 
 
 function GetIdeaSection() {
 
     return (
         <div className='raw idea-section mt-5'>
-            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/design/pexels-houzlook-com-3356416.jpg" className="d-block w-100" alt="..." />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/design/pexels-julie-aagaard-2207894.jpg" className="d-block w-100" alt="..." />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/design/pexels-mark-mccammon-1080721.jpg" className="d-block w-100" alt="..." />
-                    </div>
-                </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
+            <p className='text-center fs-3'>Get Ideas</p>
+            <div className='slide col-12 d-flex justify-content-center'>
+                <SimpleImageSlider
+                    width={500}
+                    height={504}
+                    images={images}
+                    showBullets={false}
+                    showNavs={true}
+                />
             </div>
+
         </div>
     );
 }
