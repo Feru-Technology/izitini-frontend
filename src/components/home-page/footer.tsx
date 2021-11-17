d3a3561a52444016d95c816ab75934be8b42ba34
Rama <nseorama@gmail.com>
2021-11-17 12:48:07 +0200

    add footer items

--- src/components/home-page/footer.tsx
+++ src/components/home-page/footer.tsx
@@ -13,8 +13,15 @@ function Footer() {
                         <p className="text-white">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
                             invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo</p>
                     </div>
-                    <div className='col-lg-4 col-md-4 col-sm-6'></div>
-                    <div className='col-lg-4 col-md-4 col-sm-6'></div>
+                    <div className='col-lg-4 col-md-4 col-sm-6'>
+                        <p className='fs-3 text-white'>Follow Us</p>
+                        <p className='text-white'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
+                        <img src="" alt="fbicom" />
+                    </div>
+                    <div className='col-lg-4 col-md-4 col-sm-6'>
+                        <p className='fs-3 text-white'>Contact Us</p>
+                        <p className='text-white'>My Company , 4578 Marmora Road, Glasgow D04 89GR Call us now: 0123-456-789 Email: support@whatever.com</p>
+                    </div>
                 </div>
 
                 <div className="row mt-3">
