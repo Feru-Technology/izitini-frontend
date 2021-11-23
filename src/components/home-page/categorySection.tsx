845119309545942920c043871a327b3e9e521044
Rama <nseorama@gmail.com>
2021-11-24 00:28:50 +0200

    add style small screen

--- src/components/home-page/categorySection.tsx
+++ src/components/home-page/categorySection.tsx
@@ -7,7 +7,7 @@ function CategorySection() {
 
     return (
         <div className='raw category-section mt-5'>
-            <div className=' row'>
+            <div className='row'>
                 <div className='col-lg-4 col-md-6 col-sm-12 mb-3'>
                     <div className="card col">
                         <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/Artboard%20%E2%80%93%202.png" className="card-img-top" alt="..." />
