cf5494d11858a58884bd4eca89635819d1ef53b5
Rama <nseorama@gmail.com>
2021-12-08 08:52:55 +0200

    add redux

---
+++ src/redux/reducers/user.ts
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const signup = createSlice({
+    name: 'user',
+    initialState: {
+        value: {
+            a: "", b: "",
+        }
+    },
+    reducers: {}
+})
---
+++ src/redux/stores/store.ts
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux';
+
+const store = configureStore({
+    reducer: {}
+});
