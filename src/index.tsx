f88327ad94d3fca736f835eb84acc6eea9188465
Rama <nseorama@gmail.com>
2021-12-14 23:43:56 +0200

    add 404 page

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Link, BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Link, BrowserRouter, Route, Routes, Router } from 'react-router-dom';
 import HomePage from './components/home-page/home';
 import LoginPage from './components/auth/login';
 import RegisterPage from './components/auth/register'
@@ -13,6 +13,11 @@ ReactDOM.render(
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="*" element={
+        <main style={{ padding: "1rem" }}>
+          <p>Page not found!! <Link to='/'>go back home</Link></p>
+        </main>
+      } />
     </Routes>
 
   </BrowserRouter>,
