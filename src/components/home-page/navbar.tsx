d93379642ab0dbc2926fcdcaf01318dd182dbe45
Rama <nseorama@gmail.com>
2021-12-16 10:31:37 +0200

    re size

--- src/components/home-page/navbar.tsx
+++ src/components/home-page/navbar.tsx
@@ -11,7 +11,9 @@ function NavBar() {
     <div className="">
       <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom px-5">
         <div className="container-fluid">
-          <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/Logo1.png" alt="logo" className='logo mb-2' />
+          <Link to="/" className="logo-link">
+            <img src="https://izitini-spaces.fra1.digitaloceanspaces.com/syastem-images/Logo1.png" alt="logo" className='logo mb-2' />
+          </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
