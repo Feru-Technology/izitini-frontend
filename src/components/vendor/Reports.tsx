bb9f067bfc0d2c7be1f38d9311c72e29b7b84017
Rama <nseorama@gmail.com>
2021-12-17 12:12:55 +0200

    fix side bar report

--- src/components/vendor/Reports.tsx
+++ src/components/vendor/Reports.tsx
@@ -1,7 +1,46 @@
+import React, { useState } from 'react'
+import SiderBar from './SiderBar'
+import { useMediaQuery } from 'react-responsive'
+import Header from './Header'
+import { Transition } from '@headlessui/react'
+import { Link } from 'react-router-dom'
+
 const Reports = () => {
+  const [isClosed, setIsClosed] = useState(false)
+  const isStatic = useMediaQuery({
+    query: '(min-width: 640px)',
+  })
+
   return (
-    <div>
-      Reports page ....
+    <div className='flex h-screen overflow-hidden'>
+      <SiderBar
+        isClosed={isClosed}
+        setIsClosed={setIsClosed}
+        isStatic={isStatic}
+      />
+
+      <div className='relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden'>
+        <Header
+          isClosed={isClosed}
+          setIsClosed={setIsClosed}
+          isStatic={isStatic}
+        />
+        <Transition
+          appear={true}
+          show={!isStatic && !isClosed}
+          enter='transition-opacity duration-200'
+          enterFrom='opacity-0'
+          enterTo='opacity-50'
+          leave='transition-opacity duration-200'
+          leaveFrom='opacity-50'
+          leaveTo='opacity-0'
+        >
+          <div className='fixed inset-0 bg-black opacity-60 z-10' />
+        </Transition>
+        <div>
+          Reports page ....
+        </div>
+      </div>
     </div>
   )
 }
