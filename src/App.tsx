9c688b50eb7ca1aa7fe1dfe118ddeb0d7a725cd5
Rama <nseorama@gmail.com>
2021-12-17 09:52:55 +0200

    restructure routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,52 @@
-import React, { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Main from './Main'
-import Vendor from './Vendor'
-import Profession from './Profession'
+import React, { SetStateAction, useState } from 'react'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Home } from './components/main/home'
+import Dashboard from './components/vendor/Dashboard'
+import Orders from './components/vendor/Orders'
+import Products from './components/vendor/Products'
+import Reports from './components/vendor/Reports'
+import Settings from './components/vendor/Settings'
+import Store from './components/vendor/Store'
+import Coupons from './components/vendor/Coupons'
+import CreateProduct from './components/vendor/CreateProduct'
+import SignInPage from './components/main/accounts/SignInPage'
+import CustomerSignUp from './components/main/accounts/CustomerSignUp'
+import SideBar from './components/vendor/SiderBar'
 
 function App() {
     return (
-        <div>
+        // <div>
+        <BrowserRouter>
             <Routes>
-                <Route path='/vendor' element={<Vendor />} />
-                <Route path='/' element={<Main />} />
-                <Route path='/profession' element={<Profession />} />
+                <Route path='/' element={<Home />}>
+                    <Route path='/products' element={<h1>This is all products page</h1>} />
+                    <Route path='/product' element={<h1>This is product page</h1>} />
+                </Route>
+                <Route path='/signup' element={<CustomerSignUp />} />
+                <Route path='/signin' element={<SignInPage />} />
+
+                <Route path='/vendor' element={<><SideBar isClosed={false} setIsClosed={function (value: SetStateAction<boolean>): void {
+                    throw new Error('Function not implemented.')
+                }} isStatic={false} /><Dashboard /></>} />
+                <Route path='/vendor/stores' element={<Store />} />
+                <Route
+                    path='/vendor/create-product'
+                    element={<CreateProduct />}
+                />
+                <Route path='/vendor/products' element={<Products />} />
+                <Route path='/vendor/orders' element={<Orders />} />
+                <Route path='/vendor/coupons' element={<Coupons />} />
+                <Route path='/vendor/reports' element={<Reports />} />
+                <Route path='/vendor/settings' element={<Settings />} />
+
+                <Route path='/profession' element={<h1>Professional page coming soon</h1>} />
+                <Route path='/test' element={<h2>test</h2>}>
+                    <Route path='test1' element={<h2>test1</h2>} />
+                </Route>
             </Routes>
-        </div>
+
+        </BrowserRouter>
+        // </div>
     )
 }
 
--- src/Profession.tsx
+++ src/Profession.tsx
@@ -1,19 +1,20 @@
-import React, { useState } from 'react'
-import { Routes, Route, useLocation } from 'react-router-dom'
-import { Home } from './components/main/home'
+export { }
+// import React, { useState } from 'react'
+// import { Routes, Route, useLocation } from 'react-router-dom'
+// import { Home } from './components/main/home'
 
-function Profession() {
+// function Profession() {
 
 
-    return (
-        <div>
+//     return (
+//         <div>
 
-            <Routes>
-                <Route path='/' element={<h1>Professional page coming soon</h1>} />
-                <Route path='/projects' element={<p>here we go</p>} />
-            </Routes>
-        </div>
-    )
-}
+//             <Routes>
+//                 <Route path='/' element={<h1>Professional page coming soon</h1>} />
+//                 <Route path='/projects' element={<p>here we go</p>} />
+//             </Routes>
+//         </div>
+//     )
+// }
 
-export default Profession
+// export default Profession
