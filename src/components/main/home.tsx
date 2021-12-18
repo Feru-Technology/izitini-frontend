e2672b299a757cbaf19619e3d8c35a94a60cd8aa
Rama <nseorama@gmail.com>
2021-12-18 11:52:55 +0200

    burer section layout

--- src/components/main/home.tsx
+++ src/components/main/home.tsx
@@ -6,7 +6,39 @@ export const Home = () => {
     return (
         <div>
             < Navbar />
-            this the homepage
+
+            {/* first section */}
+            <div className='m-2 flex flex-row'>
+                <div className='w-1/5 bg-red-400'>
+                    <ul>
+                        <span>
+                            Buy your products
+                        </span>
+                        <li>
+                            building materials
+                        </li>
+                        <li>
+                            paint
+                        </li>
+                        <li>
+                            bath
+                        </li>
+                        <li>
+                            steel
+                        </li>
+                        <li>
+                            wood
+                        </li>
+                        <li>
+                            ceiling
+                        </li>
+                    </ul>
+                </div>
+                <div className='w-4/5 bg-yellow-300 flex flex-row'>
+                    <div className='w-3/6'>image section</div>
+                    <div className='w-3/6'>text section</div>
+                </div>
+            </div>
         </div>
     )
 }
