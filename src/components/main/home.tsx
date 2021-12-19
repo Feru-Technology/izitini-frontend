b2b99e4c3ae0ef4c80012b66478d0d8792314305
Rama <nseorama@gmail.com>
2021-12-19 08:12:55 +0200

    category section

--- src/components/main/home.tsx
+++ src/components/main/home.tsx
@@ -5,11 +5,11 @@ import { FaTools } from "react-icons/fa"
 export const Home = () => {
 
     return (
-        <div>
+        <div className='m-2'>
             < Navbar />
 
             {/* first section */}
-            <div className='m-2 flex flex-row'>
+            <div className='flex flex-row'>
                 <div className='w-1/5 bg-red-400 h-60 overflow-y-scroll'>
                     <div className='flex flex-row justify-center'>
                         <FaTools className="h-4 w-4 mr-1 mt-1" aria-hidden="true" />
@@ -58,6 +58,27 @@ export const Home = () => {
                 <div className='w-4/5 bg-yellow-300 flex flex-row'>
                     <div className='w-3/6'>image section</div>
                     <div className='w-3/6'>text section</div>
+                </div>
+            </div>
+
+            {/* section 2 */}
+
+            <div>
+                <div className='w-1/5'>
+                    <img className='h-32 w-100 bg-gray-400' src="" alt="" />
+                    <ul>
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                    </ul>
                 </div>
             </div>
         </div>
