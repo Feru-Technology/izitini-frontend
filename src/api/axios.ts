a8ffaa3378f621f9f0d4eca585f441839848d394
Rama <nseorama@gmail.com>
2022-01-03 08:52:55 +0200

    configure axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,1 +1,18 @@
-export { }
+import axios from 'axios'
+
+interface ServerResponse {
+    data: ServerData
+}
+
+interface ServerData {
+    foo: string
+    bar: number
+}
+
+const baseUrl = 'http://localhost:3002'
+
+const Axios: any = {}
+
+Axios.instance = axios
+
+export default Axios.instance
