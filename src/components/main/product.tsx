6fe14f78b362ee19c8e42f83a4fba8532b83735d
Rama <nseorama@gmail.com>
2022-01-19 10:10:55 +0200

    add productimage

--- src/components/main/product.tsx
+++ src/components/main/product.tsx
@@ -24,6 +24,36 @@ const Product = () => {
 
     // const { isLoading, product } = useSelector((state: RootState) => state.storeProduct);
 
+
+    const data = [
+        {
+            image: 'https://media.istockphoto.com/photos/cement-bags-pile-picture-id476199756?k=20&m=476199756&s=612x612&w=0&h=AHEdPIf2xyl3amOyAgG9mUwp4WRS3GgO-SzyElhDx4A=',
+
+        },
+        {
+            image: 'https://media.istockphoto.com/photos/detail-of-broken-pipe-picture-id1074493878',
+
+        },
+        {
+            image: 'https://media.istockphoto.com/photos/cement-bags-pile-picture-id476199756?k=20&m=476199756&s=612x612&w=0&h=AHEdPIf2xyl3amOyAgG9mUwp4WRS3GgO-SzyElhDx4A=',
+
+        },
+        {
+            image: 'https://media.istockphoto.com/photos/sprinkler-installation-in-a-field-of-park-picture-id1043550948',
+
+        },
+        {
+            image: 'https://media.istockphoto.com/photos/cement-bags-pile-picture-id476199756?k=20&m=476199756&s=612x612&w=0&h=AHEdPIf2xyl3amOyAgG9mUwp4WRS3GgO-SzyElhDx4A=',
+
+        },
+        {
+            image: 'https://media.istockphoto.com/photos/rusty-burst-pipe-in-baku-botanic-garden-picture-id500598328',
+
+        },
+    ]
+
+    const [displayImage, setDisplayImage] = useState(data[0].image);
+
     return (
         <>
 
@@ -37,61 +67,37 @@ const Product = () => {
                                 <div className="flex">
 
                                     <div>
+                                        {data.map((image) => (<div className="my-1 hover:border-black"
+                                            onPointerOver={() => setDisplayImage(image.image)}
+                                        >
+                                            <img
+                                                src={image.image}
+                                                className="border mr-2 w-20"
+                                                alt="..."
+                                            />
+                                        </div>)
+                                        )}
 
-                                        <div className="d-flex flex-direction-column gap-2">
-                                            <img
-                                                src="https://media.istockphoto.com/photos/cement-bags-pile-picture-id476199756?k=20&m=476199756&s=612x612&w=0&h=AHEdPIf2xyl3amOyAgG9mUwp4WRS3GgO-SzyElhDx4A="
-                                                className="border mr-2 w-20"
-                                                alt="..."
-                                            />
-                                        </div>
-                                        <div className="d-flex flex-direction-column gap-2">
-                                            <img
-                                                src="https://media.istockphoto.com/photos/cement-bags-pile-picture-id476199756?k=20&m=476199756&s=612x612&w=0&h=AHEdPIf2xyl3amOyAgG9mUwp4WRS3GgO-SzyElhDx4A="
-                                                className="border mr-2 w-20"
-                                                alt="..."
-                                            />
-                                        </div>
-                                        <div className="d-flex flex-direction-column gap-2">
-                                            <img
-                                                src="https://media.istockphoto.com/photos/cement-bags-pile-picture-id476199756?k=20&m=476199756&s=612x612&w=0&h=AHEdPIf2xyl3amOyAgG9mUwp4WRS3GgO-SzyElhDx4A="
-                                                className="border mr-2 w-20"
-                                                alt="..."
-                                            />
-                                        </div>
-                                        <div className="d-flex flex-direction-column gap-2">
-                                            <img
-                                                src="https://media.istockphoto.com/photos/cement-bags-pile-picture-id476199756?k=20&m=476199756&s=612x612&w=0&h=AHEdPIf2xyl3amOyAgG9mUwp4WRS3GgO-SzyElhDx4A="
-                                                className="border mr-2 w-20"
-                                                alt="..."
-                                            />
-                                        </div>
-                                        <div className="d-flex flex-direction-column gap-2">
-                                            <img
-                                                src="https://media.istockphoto.com/photos/cement-bags-pile-picture-id476199756?k=20&m=476199756&s=612x612&w=0&h=AHEdPIf2xyl3amOyAgG9mUwp4WRS3GgO-SzyElhDx4A="
-                                                className="border mr-2 w-20"
-                                                alt="..."
-                                            />
-                                        </div>
                                     </div>
 
                                     <div className="">
                                         <img
-                                            src="https://media.istockphoto.com/photos/cement-bags-pile-picture-id476199756?k=20&m=476199756&s=612x612&w=0&h=AHEdPIf2xyl3amOyAgG9mUwp4WRS3GgO-SzyElhDx4A="
+                                            src={displayImage}
                                             object-fit="false"
-                                            className=""
+                                            className=" max-h-96 justify-right"
                                             alt="product pic"
                                         />
                                     </div>
+
                                 </div>
 
                                 {/* tabs and details */}
                                 <div className="container">
                                     {/* tabs */}
                                     <div className="">
-                                        <ul className="flex space-x-5 justify-center font-bold text-lg" style={{ color: "#000" }}>
+                                        <ul className="flex space-x-5 justify-center font-bold text-lg">
                                             <li>Description</li>
-                                            <li>Technical & specification</li>
+                                            <li>Products Specification</li>
                                             <li>Review</li>
                                             <li>Shipping & Return</li>
                                         </ul>
@@ -103,13 +109,12 @@ const Product = () => {
                                             <p>test 1</p>
                                             <p>test 2</p>
                                         </div>
-                                        <div></div>
                                     </div>
                                 </div>
                             </div>
                             <div className="w-1/3">
                                 <div className='text-base space-y-3'>
-                                    <h2 className="font-bold text-3xl">Product Name</h2>
+                                    <h2 className=" text-3xl">Product Name</h2>
                                     <p>name of the store</p>
                                     <span className="flex text-4xl"
                                         style={{ color: "#ff9900" }}
@@ -129,9 +134,10 @@ const Product = () => {
                                         <i>
                                             <AiFillStar />
                                         </i>
+                                        <span className='ml-3 text-black text-2xl'> (28)</span>
                                     </span>
-                                    <span> (28)</span>
-                                    <p className="font-bold">25,000 RWF</p>
+                                    <p className="text-lg">25,000 RWF</p>
+                                    <p>read to shop in Kigali</p>
                                     <p>xx in the store</p>
                                     <p>
                                         Lorem Ipsum is simply dummy text of the dummy text ever since
@@ -140,7 +146,7 @@ const Product = () => {
                                     <div className="flex space-x-12">
                                         <div className="w-24">
                                             <select
-                                                className='w-32 h-9 rounded border-2 bg-white'
+                                                className='w-32 h-9 rounded border-2 bg-white px-3'
                                                 aria-label="multiple select example"
                                             >
                                                 <option value="1">1</option>
