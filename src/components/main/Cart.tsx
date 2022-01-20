1e29642d4a3098add1986bc824047b78b18a0701
Rama <nseorama@gmail.com>
2022-01-20 13:10:55 +0200

    add product list

--- src/components/main/Cart.tsx
+++ src/components/main/Cart.tsx
@@ -18,7 +18,106 @@ import {
 const Cart = () => {
 
     return (<>
-        my Cart
+        <Navbar />
+        <div className='bg-gray-100 h-full'>
+            <p>Your cart</p>
+            <div className='md:flex md:p-12 md:space-x-5'>
+                <div className='w-2/3 bg-black'>
+
+                    <table className='min-w-full divide-y divide-gray-200'>
+                        <thead className='bg-white'>
+                            <tr>
+                                <th
+                                    scope='col'
+                                    className='px-6 py-3 text-left text-xs font-medium text-gray-700
+                                    uppercase tracking-wider'
+                                >
+                                    Product
+                                </th>
+                                <th
+                                    scope='col'
+                                    className='px-6 py-3 text-left text-xs font-medium text-gray-700
+                                    uppercase tracking-wider items-center'
+                                >
+                                    Price
+                                </th>
+                                <th
+                                    scope='col'
+                                    className='px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider'
+                                >
+                                    Quality
+                                </th>
+                                <th
+                                    scope='col'
+                                    className='px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider'
+                                >
+                                    Total
+                                </th>
+                                <th
+                                    scope='col'
+                                    className='relative px-6 py-3'
+                                >
+                                    <span className='sr-only'>
+                                        Edit
+                                    </span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody className='bg-white'>
+                            {/* {
+                                                // isLoading
+                                                //     ? (<h1>loading ...</h1>)
+                                                //     : (products.map((product) => ( */}
+                            <tr
+                            // onClick={e => activeProduct(product)}
+                            >
+                                <td className='px-6 py-4'>
+                                    <div className='flex items-center'>
+                                        <div className='flex-shrink-0 h-10 w-10'>
+                                            <img
+                                                className='h-10 w-10'
+                                                src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60'
+                                                alt=''
+                                            />
+                                        </div>
+                                        <div className='ml-4 text-sm font-medium text-gray-800'>
+                                            ab
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className='px-6 py-4 text-sm text-gray-800'>
+                                    abc
+                                </td>
+                                <td className='px-6 py-4 text-xs text-gray-800'>
+                                    <div className=' rounded border-2 w-24'>
+                                        <div className='flex justify-center py-1'>
+                                            <button>+</button>
+                                            <span className='mx-4 font-semibold'>1</span>
+                                            <button>-</button>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className='px-6 py-4 text-sm text-gray-800'>
+                                    abcde
+                                </td>
+                                <td className='px-6 py-4 text-right text-sm font-medium'>
+                                    <a
+                                        href='#'
+                                        className='text-indigo-600 hover:text-indigo-900'
+                                    >
+                                        Edit
+                                    </a>
+                                </td>
+                            </tr>
+                            {/* // )))} */}
+
+                        </tbody>
+                    </table>
+                </div>
+                <div className='w-1/3 bg-gray-500'></div>
+            </div>
+        </div>
+        <Footer />
     </>
     )
 }
