dd9ee280da42ba0a5006639a2b106ddc5abc051c
Rama <nseorama@gmail.com>
2022-02-16 08:55:55 +0200

    add account types component

---
+++ src/components/main/accountTypes.tsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom'
+
+export const AccountTypes = () => {
+    return (
+        <div className='bg-dark-blue pb-2 md:py-5 lg:py-10 text-white'>
+            who are you
+        </div>
+    )
+}
