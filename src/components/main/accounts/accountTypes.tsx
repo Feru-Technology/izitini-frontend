3386cec898fb7c37d7b7f607723dba6e09bc0a07
Rama <nseorama@gmail.com>
2022-02-16 10:15:55 +0200

    responsivess

--- src/components/main/accounts/accountTypes.tsx
+++ src/components/main/accounts/accountTypes.tsx
@@ -10,38 +10,38 @@ const AccountTypes = () => {
                         <Link to={'/'}>
                             <img
                                 src='https://izitini-spaces.fra1.digitaloceanspaces.com/system-images/Logo1.png'
-                                className=''
-                                width='100px'
-                                height='100px'
+                                className='w-auto h-12 md:h-16 lg:h-20 xl:h-24'
                                 alt='logo'
                             />
                         </Link>
                     </h1>
                 </div>
                 <div className='flex justify-center my-6'>
-                    <span className='font-serif font-light text-xl'>Who are you?</span>
+                    <span className='font-serif font-light text-xl md:text-2xl'>Who are you?</span>
                 </div>
 
-                <div className='md:flex justify-center font-sans font-extralight'>
-                    <div className='bg-gray-100 mx-5 p-5 border-2 border-gray-200 my-5
+                <div className='md:flex justify-center font-sans font-extralight md:text-lg'>
+                    <div className='bg-gray-100 mx-5 p-5 border-2 border-gray-200 my-5 md:w-72
                     hover:bg-dark-blue hover:text-white'>
                         <span className='flex justify-center font-normal'>Customer</span>
                         <p className='flex justify-center'>I want to buy construction tools for my home</p>
                     </div>
 
-                    <div className='bg-gray-100 mx-5 p-5 border-2 border-gray-200 my-5
+                    <div className='bg-gray-100 mx-5 p-5 border-2 border-gray-200 my-5 md:w-72
                     hover:bg-dark-blue hover:text-white'>
                         <span className='flex justify-center font-normal'>Professional</span>
                         <p className='flex justify-center'>I fix peoples homes</p>
                     </div>
+                </div>
 
-                    <div className='bg-gray-100 mx-5 p-5 border-2 border-gray-200 my-5
+                <div className='md:flex justify-center font-sans font-extralight md:text-lg'>
+                    <div className='bg-gray-100 mx-5 p-5 border-2 border-gray-200 my-5 md:w-72
                     hover:bg-dark-blue hover:text-white'>
                         <span className='flex justify-center font-normal'>Contractor</span>
                         <p className='flex justify-center'>I own a construction company</p>
                     </div>
 
-                    <div className='bg-gray-100 mx-5 p-5 border-2 border-gray-200 my-5
+                    <div className='bg-gray-100 mx-5 p-5 border-2 border-gray-200 my-5 md:w-72
                     hover:bg-dark-blue hover:text-white'>
                         <span className='flex justify-center font-normal'>Vendor</span>
                         <p className='flex justify-center'>I own a construction shop</p>
