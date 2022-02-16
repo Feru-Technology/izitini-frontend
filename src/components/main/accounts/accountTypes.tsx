1f5ac74c6cb5b5c4253a9cb6c88720c2ae57f3d6
Rama <nseorama@gmail.com>
2022-02-16 09:35:55 +0200

    add account types

--- src/components/main/accounts/accountTypes.tsx
+++ src/components/main/accounts/accountTypes.tsx
@@ -18,14 +18,30 @@ const AccountTypes = () => {
                         </Link>
                     </h1>
                 </div>
-                <div className='flex justify-center'>
+                <div className='flex justify-center my-6'>
                     <span>Who are you?</span>
                 </div>
 
                 <div className='flex justify-center'>
-                    <div>Customer</div>
-                    <div>Vendor</div>
-                    <div>Profession</div>
+                    <div className='bg-gray-100 mx-5 p-10 border-2 border-gray-200'>
+                        <span>Customer</span>
+                        <p>I want to buy construction tools for my home</p>
+                    </div>
+
+                    <div className='bg-gray-100 mx-5 p-10 border-2 border-gray-200'>
+                        <span>Professional</span>
+                        <p>I am fix peoples homes</p>
+                    </div>
+
+                    <div className='bg-gray-100 mx-5 p-10 border-2 border-gray-200'>
+                        <span>Contractor</span>
+                        <p>I own a construction company</p>
+                    </div>
+
+                    <div className='bg-gray-100 mx-5 p-10 border-2 border-gray-200'>
+                        <span>Vendor</span>
+                        <p>I own a construction shop</p>
+                    </div>
                 </div>
             </div>
         </div>
