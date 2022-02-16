5ad9c5d7920e17db63f542d48147a3d52cb7bc81
Rama <nseorama@gmail.com>
2022-02-16 09:45:55 +0200

    add some css

--- src/components/main/accounts/accountTypes.tsx
+++ src/components/main/accounts/accountTypes.tsx
@@ -4,7 +4,7 @@ const AccountTypes = () => {
     return (
         <div>
 
-            <div className='w-full lg:w-5/12 mx-auto pt-6'>
+            <div className='w-full mx-auto pt-6'>
                 <div>
                     <h1 className='my-6 flex justify-center'>
                         <Link to={'/'}>
@@ -22,24 +22,24 @@ const AccountTypes = () => {
                     <span>Who are you?</span>
                 </div>
 
-                <div className='flex justify-center'>
+                <div className='md:flex justify-center'>
                     <div className='bg-gray-100 mx-5 p-10 border-2 border-gray-200'>
-                        <span>Customer</span>
+                        <span className='flex justify-center'>Customer</span>
                         <p>I want to buy construction tools for my home</p>
                     </div>
 
                     <div className='bg-gray-100 mx-5 p-10 border-2 border-gray-200'>
-                        <span>Professional</span>
-                        <p>I am fix peoples homes</p>
+                        <span className='flex justify-center'>Professional</span>
+                        <p>I fix peoples homes</p>
                     </div>
 
                     <div className='bg-gray-100 mx-5 p-10 border-2 border-gray-200'>
-                        <span>Contractor</span>
+                        <span className='flex justify-center'>Contractor</span>
                         <p>I own a construction company</p>
                     </div>
 
                     <div className='bg-gray-100 mx-5 p-10 border-2 border-gray-200'>
-                        <span>Vendor</span>
+                        <span className='flex justify-center'>Vendor</span>
                         <p>I own a construction shop</p>
                     </div>
                 </div>
