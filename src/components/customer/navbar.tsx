99c31a8e41a6545b466c590fc2e2444e4e920ecf
Rama <nseorama@gmail.com>
2022-02-19 10:10:55 +0200

    fix navbar profile

--- src/components/customer/navbar.tsx
+++ src/components/customer/navbar.tsx
@@ -199,7 +199,7 @@ export const Navbar = () => {
                                                     leaveFrom="transform opacity-100 scale-100"
                                                     leaveTo="transform opacity-0 scale-95"
                                                 >
-                                                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-4/5 rounded-md shadow-lg p-2 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                                                         <Menu.Item>
                                                             {({ active }) => (
                                                                 <Link to='/profile'
