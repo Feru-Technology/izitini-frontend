2a622e91e4fb746d2db3810e0e3cdf6612f778f1
Rama <nseorama@gmail.com>
2022-04-25 19:47:10 +0200

    image uploading and fetching functionalities

---
+++ src/redux/image/image.interfaces.ts
@@ -0,0 +1,12 @@
+
+export interface HTTPError {
+    status: number
+    message: string
+}
+export interface IImage {
+    id: string
+    note: string
+    image_url: string
+    createdAt: Date,
+    updatedAt: Date
+}
