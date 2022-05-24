bf49398959c8fa315bae6446dc7053a5754fa63f
Rama <nseorama@gmail.com>
2022-05-24 11:59:18 +0200

    fix product image

--- src/components/customer/order.tsx
+++ src/components/customer/order.tsx
@@ -61,8 +61,8 @@ const Order = () => {
                         <div className='fixed inset-0 bg-black opacity-60 z-10' />
                     </Transition>
 
-                    {/* customer orders */}
-                    <div className='px-2 md:px-6 lg:px-14 w-full'>
+                    {/* customer order */}
+                    <div className=' md:px-6 lg:px-14 w-full'>
                         <div className='flex justify-between text-sm md:text-base my-4 mx-2'>
                             <p className='font-bold '>My Order</p>
                             <p> <span className='font-bold'> status:</span> <span className='px-2 py-1 text-white bg-dark-blue rounded-lg'>{order?.status}</span> </p>
@@ -70,7 +70,7 @@ const Order = () => {
                         <div className='bg-white border border-gray-200 rounded-md'>
 
                             {order ?
-                                <div className='w-full my-4 px-4 md:my-5 md:px-5 lg:my-6 lg:px-6'>
+                                <div className='w-full p-0.5 md:my-5 md:px-5 lg:my-6 lg:px-6'>
                                     <table className='w-full border-gray-200 text-gray-600'>
                                         <thead className='bg-gray-100'>
                                             <tr className='font-bold text-xs md:text-sm text-center border uppercase'>
@@ -79,23 +79,23 @@ const Order = () => {
                                                     className='
                                                 py-3 lg:text-base
                                     '
-                                                >Product</th>
+                                                >Products</th>
                                                 <th
                                                     scope='col'
                                                     className='
-                                                py-3 lg:text-base
+                                                py-3 lg:text-base sr-only md:not-sr-only
                                     '
                                                 >Price</th>
                                                 <th
                                                     scope='col'
                                                     className='
-                                                py-3 lg:text-base
+                                                py-3 lg:text-base sr-only md:not-sr-only
                                     '
-                                                >quantity</th>
+                                                >Quantity</th>
                                                 <th
                                                     scope='col'
                                                     className='
-                                                py-3 lg:text-base
+                                                py-3 lg:text-base sr-only lg:not-sr-only
                                     '
                                                 >Notes</th>
                                             </tr>
@@ -104,23 +104,53 @@ const Order = () => {
                                         <tbody>
                                             {order.order_items.map((item) => {
                                                 return (
-                                                    <tr className='border'>
-                                                        <td className='text-center'>
-                                                            <div className='flex'>
-                                                                <img className='h-20 p-1  2xl:h-52'
-                                                                    src={item.product.display_image || 'https://izitini-spaces.fra1.digitaloceanspaces.com/Screenshot%20from%202021-11-30%2010-21-50.png'} alt='' />
-                                                                {item.product.name}
+                                                    <tr className='border text-gray-900'>
+                                                        <td className='md:w-2/4'>
+                                                            <div className='md:flex p-3'>
+                                                                <div className='w-8/12 md:w-1/3 md:p-1'>
+                                                                    <img className=' 2xl:h-52 w-full'
+                                                                        src={item.product.display_image || 'https://izitini-spaces.fra1.digitaloceanspaces.com/Screenshot%20from%202021-11-30%2010-21-50.png'} alt='' />
+                                                                </div>
+                                                                <div className='md:w-2/3 md:px-3'>
+                                                                    <ul className='font-normal text:xs md:text-base text-gray-800'>
+                                                                        <li className='font-bold text:sm text-lg'>{item.product.name}</li>
+                                                                        <li>By <span className='text-sm italic text-gray-500'>product Brand</span> </li>
+                                                                        <li className='md:sr-only'>Price <span className='text-sm italic text-gray-500'>1200 Frw</span></li>
+                                                                        <li>Color <span className='text-sm italic text-gray-500'>Red</span></li>
+                                                                        <li>Size <span className='text-sm italic text-gray-500'>Lg</span></li>
+                                                                        <li className='md:sr-only'>Qty <span className='text-sm italic text-gray-500'>2</span></li>
+                                                                        <li className='lg:sr-only'>Note <span className='text-sm italic text-gray-500'>note</span></li>
+                                                                    </ul>
+
+                                                                </div>
                                                             </div>
                                                         </td>
-                                                        <td className='text-center'>{item.product.price}</td>
-                                                        <td className='text-center'>{item.quantity}</td>
-                                                        <td className='text-center'>{item.details}</td>
+                                                        <td className='text-center sr-only md:not-sr-only'>{item.product.price}</td>
+                                                        <td className='text-center sr-only md:not-sr-only'>{item.quantity}</td>
+                                                        <td className='text-center sr-only lg:not-sr-only'>{item.details}</td>
                                                     </tr>
                                                 )
                                             })}
                                         </tbody>
 
                                     </table>
+
+                                    <div className='mt-5 border'>
+                                        <h1 className='border-b'> <span className='px-2 py-3 font-bold text-lg'>More Details</span> </h1>
+                                        <ul className='space-y-2 px-2 py-3 font-semibold text-base text-gray-500'>
+                                            <li>Order no: <span className='font-normal ml-2 text-gray-900'>1</span></li>
+                                            <li>Type: <span className='font-normal ml-2 text-gray-900'>Sample</span></li>
+                                            <li>Is Paid: <span className='font-normal ml-2 text-gray-900'>Yes</span></li>
+                                            <li>Paid Amount: <span className='font-normal ml-2 text-gray-900'>1000</span></li>
+                                            <li>Payment method: <span className='font-normal ml-2 text-gray-900'>card</span></li>
+                                            <li>Total Order cost: <span className='font-normal ml-2 text-gray-900'>20000</span></li>
+                                            <li>Total Shipping cost: <span className='font-normal ml-2 text-gray-900'>5000</span></li>
+                                            <li>Shipping method: <span className='font-normal ml-2 text-gray-900'>transit</span></li>
+                                            <li>Shipping address: <span className='font-normal ml-2 text-gray-900'>123456 ave</span></li>
+
+                                        </ul>
+
+                                    </div>
 
                                 </div> : <div></div>}
 
--- src/components/vendor/Products.tsx
+++ src/components/vendor/Products.tsx
@@ -168,7 +168,7 @@ const Products = () => {
                                                                     <div className='flex-shrink-0 h-10 w-10'>
                                                                         <img
                                                                             className='h-10 w-10'
-                                                                            src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60'
+                                                                            src={product.display_image || 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60'}
                                                                             alt=''
                                                                         />
                                                                     </div>
