7f0c79a77e8630ff5c21afdb6f6c1dbb8ed17a66
Rama <nseorama@gmail.com>
2022-06-10 15:28:56 +0200

    cart action

--- src/api/orders.ts
+++ src/api/orders.ts
@@ -1,3 +1,14 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { cart, cartFailed } from '../redux/order/cart'
+import { post, fetch, update, destroy } from './apiAction'
 
+const token = localStorage.getItem('token')
 
-export { }
+export const useCart = (operation: string, order_id: string, product_id: string, route?: string) => {
+    const dispatch = useDispatch()
+    let op
+    operation === 'update'
+        ? op = update
+        : op = destroy
+    return op(dispatch, cart, cartFailed, `/orders/${route}/${order_id}/${product_id}`, {}, token)
+}
